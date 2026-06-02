fb723ac8298c46844aed43fbbeee84ad9d11a75f
unknown <aldmyatyfrhh@gmail.com>
2026-06-02 08:33:01 +0300

    update

--- src/Modules/payment/payment.module.ts
+++ src/Modules/payment/payment.module.ts
@@ -5,12 +5,16 @@ import { PaymentService } from "./payment.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Payment } from "./payment.entity";
 import { SubscriptionModule } from "../subscription/subscription.module";
+import { JwtModule } from "@nestjs/jwt";
+import { UserModule } from "../Users/user.module";
 
 @Module({
   providers: [PaymentService],
   controllers: [PaymentController],
   imports: [
     PaymobModule,
+    JwtModule,
+    UserModule,
     forwardRef(() => SubscriptionModule),
     TypeOrmModule.forFeature([Payment]),
   ],
